8093207d6f77894aa3baddb242364eb76652fe3f
karamawariken <24karamawariken@gmail.com>
2017-06-16 08:36:24 +0900

    stateがpublicのものしか表示しないように編集

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @products = Product.find_public_products
   end
 
   def show
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,8 @@ class Product < ActiveRecord::Base
   mount_uploader :image1_link, ImageUploader
   mount_uploader :image2_link, ImageUploader
   mount_uploader :image3_link, ImageUploader
+
+  def self.find_public_products
+    products = Product.where(:state => "public")
+  end
 end
